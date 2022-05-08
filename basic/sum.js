6c992c3970f69750cbf7bc13c08aedc880503de2
Angelia <angelia.yingru@gmail.com>
2022-05-08 14:35:13 +0800

    sum

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+// 回傳 1 + 2 + ... + n 的結果
+function sum(n) {
+  
+  // return 結果
+}
+
+for(let i = 1; i <= n; i++){
+  sum = sum + i;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100)); // 5050 

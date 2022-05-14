f5bc42cf243942830f240fcba066025b7fde20c7
Angelia <angelia.yingru@gmail.com>
2022-05-14 15:12:52 +0800

    sucess

--- basic/sum.js
+++ basic/sum.js
@@ -1,9 +1,21 @@
 // 回傳 1 + 2 + ... + n 的結果
+function sum1(n) {
+  let sum = 0;
+  for(let i = 1; i <= n; i++){
+    sum = sum + i;
+  }
+  return sum;
+  // return 結果
+}
 
-let sum = 0;
-let n = 10;
-for(let i = 1; i <= n; i++){
-  sum = sum + i;
+
+function sum2(n){
+  return ((n+1)*n)/2;
+}
+
+
+function sum3(n) {
+  [1,2,3,...n].reduce((sum,item) => sum +item,0);
 }
 
 
